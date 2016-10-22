07cc3967224b5284e9dabb3329d3b2f5f5c47869
Edward Bennigsen <pe.ads0@gmail.com>
2016-10-22 14:43:12 +0100

    Example code succesfully downloads video and extracts audio

---
+++ youtube-dl-example.py
@@ -0,0 +1,37 @@
+# Based on an example from https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
+
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+url = 'https://www.youtube.com/watch?v=thO9KJJBGpU'
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Finished")
